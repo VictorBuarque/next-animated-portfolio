c988211d9dc232a14a6399566f6610c3a04931f8
Victor Buarque <vcgabriel38@gmail.com>
2024-02-12 20:44:05 -0300

    feat: implement header desktop e mobile

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,17 +1,17 @@
-
-import type { LinkShape } from "./partials/navigation/Navigation"
-import HeaderDesktop from "./HeaderDesktop"
+import type { LinkShape } from "./partials/navigation/Navigation";
+import HeaderDesktop from "./HeaderDesktop";
+import HeaderMobile from "./HeaderMobile";
 
 export default function Header() {
-  const links: LinkShape[]= [
-    {label: 'About', href: '/about'},
-    {label: 'Portfolio', href: '/portfolio'},
-    {label: 'Contact', href: '/contact'},
-  ]
+  const links: LinkShape[] = [
+    { label: "About", href: "/about" },
+    { label: "Portfolio", href: "/portfolio" },
+    { label: "Contact", href: "/contact" },
+  ];
   return (
-    <div className='h-full flex items-center justify-between py-10'>
-        <HeaderDesktop links={links}/>
+    <div className="h-full flex items-center justify-between py-10">
+      <HeaderDesktop links={links} />
+      <HeaderMobile links={links} />
     </div>
-  )
+  );
 }
-
