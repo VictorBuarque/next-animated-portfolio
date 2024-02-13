fc7148d0e712630c3137fd1212bb897a5f56623f
Victor Buarque <vcgabriel38@gmail.com>
2024-02-13 00:07:53 -0300

    styles: increment hover style

--- src/components/header/partials/navigation/Navigation.tsx
+++ src/components/header/partials/navigation/Navigation.tsx
@@ -15,7 +15,7 @@ export default function Navigation({ links }: NavProps) {
     <nav className="flex flex-row justify-between items-center w-full gap-8 mr-10 h-[24px] xl:text-xl 2xl:text-[20px]">
       {links.map((link) => (
         <Link key={link.label} href={link.href}>
-          <span className='className="whitespace-nowrap text-md font-bold'>
+          <span className='className="whitespace-nowrap text-md font-bold hover:text-sky-500 hover:bg-white hover:rounded-md'>
             {link.label}
           </span>
         </Link>
